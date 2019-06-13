ffc68a163d641d880d55a26f62eb814a41abca63
KamilStepien <stepienkamil97@gmail.com>
2019-06-13 22:45:12 +0200

    First Commit

---
+++ MyCar/MyCar/StartPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyCar
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StartPage : ContentPage
+	{
+		public StartPage ()
+		{
+			InitializeComponent ();
+		}
+        public async void exit(object sender, EventArgs e)
+        {
+
+        }
+
+        public async void contnue(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void start(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
